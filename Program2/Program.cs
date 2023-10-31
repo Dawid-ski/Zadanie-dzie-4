4df9325690f83815b8f8b4739ee5a4758a0c9c67
Dawid-ski <dawid258@wp.pl>
2023-10-31 23:26:08 +0100

    Dodaj pliki projektów.

---
+++ Program2/Program.cs
@@ -0,0 +1,20 @@
+﻿var name = "Ewa";
+var Age = 35;
+var Gender = "Kobieta";
+
+if (name == "Ewa" && Age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+if (Gender == "Kobieta" && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+if (Gender == "Kobieta" && Age > 30)
+{ 
+    Console.WriteLine("kobieta powyżej 30 lat");
+}
+else if (Gender == "Mężczyzna" && Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
